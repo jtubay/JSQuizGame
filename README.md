967bdf81cf1d87d8c4ec539f1ec2de23ac28cb08
Jean Tubay <jean.tubay.92@gmail.com>
2019-10-23 22:49:51 -0400

    added timer

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # JSQuizGame
+
+The project was meant to be a quiz game that would keep a timer and a score. I got the timer to work along with some dom manipulation. I will continue to update it as i figure it our.
+
+
+
+Below are links to the current version and the repository:
--- script.js
+++ script.js
@@ -1,10 +1,32 @@
+//global variables defined for ease
+
 var startBtn = document.getElementById("start-button");
 var questionHeading = document.getElementById("questions");
 var answersList = document.getElementById("answers");
+var timeEl = document.getElementById('time');
+let secondsLeft = 60;
 
+
+//timer function
+function setTime () {
+    var timerInterval = setInterval(function(){
+      secondsLeft --;
+      timeEl.textContent = "You have " + secondsLeft + " seconds left"
+  
+      if(secondsLeft === 0) {
+        clearInterval(timerInterval);
+        document.getElementById("qa-screen").classList.add("d-none")
+        document.getElementById("end-screen").classList.remove("d-none")
+      }
+  
+    }, 1000)
+  };
+
+//event that triggers the game and hides the start screen
 startBtn.addEventListener("click", function(event) {
     document.getElementById("start-screen").classList.add("d-none")
     document.getElementById("qa-screen").classList.remove("d-none")
+    setTime();
 
     questionHeading.textContent = questions[0].title;
     answersList.innerHTML = " ";
